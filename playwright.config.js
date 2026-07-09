d815cef19bb68930cfa22a4962acb1a17cfbe731
David <temiloluwaw@gmail.com>
2026-07-09 23:32:04 +0100

    updated the config with timeout section added a test to Test Login to Rahul Shetty Academy

--- playwright.config.js
+++ playwright.config.js
@@ -25,6 +25,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -7,7 +7,7 @@ test("First Test", async ({ page }) => {
   expect(pagetitle).toBe("Google");
 });
 
-test.only("Testing Login to Rahul Shetty Academy with Invalid Credentials", async ({
+test("Testing Login to Rahul Shetty Academy with Invalid Credentials", async ({
   page,
 }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
@@ -18,10 +18,27 @@ test.only("Testing Login to Rahul Shetty Academy with Invalid Credentials", asyn
   const password = page.locator("#password");
   const signInButton = page.locator("#signInBtn");
 
-  await username.type("invalidUser");
-  await password.type("invalidPassword");
+  await username.fill("invalidUser");
+  await password.fill("invalidPassword");
   await signInButton.click();
 
   const errorMessage = page.locator(".alert-danger");
   await expect(errorMessage).toHaveText("Incorrect username/password.");
+});
+
+test("Testing Login to Rahul Shetty Academy", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const pagetitle = await page.title();
+  expect(pagetitle).toBe("LoginPage Practise | Rahul Shetty Academy");
+
+  const username = page.locator("#username");
+  const password = page.locator("#password");
+  const signInButton = page.locator("#signInBtn");
+
+  await username.fill("rahulshettyacademy");
+  await password.fill("Learning@830$3mK2");
+  await signInButton.click();
+
+  const firstItem = await page.locator(".card-body a").nth(0).textContent();
+  expect(firstItem).toBe("iphone X");
 });
